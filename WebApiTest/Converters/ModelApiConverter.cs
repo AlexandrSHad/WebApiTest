d3e18e44706d4848f6eabfe2e6d445619d93ae63
Alexandr SHad <shadurskij@gmail.com>
2018-02-13 05:50:32 +0200

    Create method improved

--- WebApiTest/Converters/ModelApiConverter.cs
+++ WebApiTest/Converters/ModelApiConverter.cs
@@ -12,32 +12,16 @@ namespace WebApiTest.Converters
 {
     public class ModelApiConverter : JsonCreationConverter<IModel>
     {
-        //private readonly Dictionary<ModelTypes, Type> _typesDictionary = new Dictionary<ModelTypes, Type> {
-        //    { ModelTypes.First,  typeof(Model1) },
-        //    { ModelTypes.Second, typeof(Model2) }
-        //};
+        private readonly Dictionary<ModelTypes, Type> _typesDictionary = new Dictionary<ModelTypes, Type> {
+            { ModelTypes.First,  typeof(Model1) },
+            { ModelTypes.Second, typeof(Model2) }
+        };
 
         public override IModel Create(Type objectType, JObject jObject)
         {
             ModelTypes modelType = (ModelTypes)jObject.Value<int>("modelType");
 
-            IModel obj;
-
-            switch (modelType)
-            {
-                case ModelTypes.First:
-                    obj = new Model1();
-                    break;
-                case ModelTypes.Second:
-                    obj = new Model2();
-                    break;
-                default:
-                    throw new ArgumentException();
-            }
-
-            //IModel obj = (IModel)jObject.ToObject(_typesDictionary[modelType]);
-
-            return obj;
+            return (IModel)jObject.ToObject(_typesDictionary[modelType]);
         }
     }
 }
